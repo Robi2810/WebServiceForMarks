bc3f761c316724c43132db56b77c4f40407e75fc
kvishnyakov <kvishnyakov@uma.tech>
2024-03-18 12:11:14 +0300

    NEW MODEL

--- WebSite/markapp/models.py
+++ WebSite/markapp/models.py
@@ -1,68 +1,47 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
-class Department(models.Model):
-    dept_name = models.CharField(max_length=20, primary_key=True)
-    building = models.CharField(max_length=40, null=True)
-    budget = models.IntegerField(null=True)
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks', null=True, blank=True)
+    title = models.CharField(max_length=35)
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
-        return self.dept_name
+        return self.title
 
 
-class Group(models.Model):
-    group_id = models.CharField(primary_key=True, max_length=10)
-    title = models.CharField(max_length=50, null=True)
-    dept_name = models.ForeignKey(Department, on_delete=models.CASCADE)
-    credits = models.IntegerField(null=True)
-
-    def __str__(self):
-        return self.group_id
-
-
-class Section(models.Model):
-    group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
-    sec_id = models.CharField(primary_key=True, max_length=10)
-    semester = models.CharField(max_length=10, null=True)
-    year = models.CharField(max_length=10, null=True)
-    section_buidling = models.CharField(max_length=40, null=True)
+class Achievement(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='achievement', null=True, blank=True)
+    title = models.CharField(max_length=35)
+    description = models.TextField(null=True, blank=True)
+    weight = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.sec_id
+        return self.title
+
+    def __int__(self):
+        return self.weight
 
 
-class Emploeye(models.Model):
-    emp_id = models.CharField(primary_key=True, max_length=10)
-    emp_name = models.CharField(max_length=50)
-    dept_name = models.ForeignKey(Department, on_delete=models.CASCADE)
-    tot_cred = models.IntegerField(null=True)
-
-    def __str__(self):
-        return self.emp_id
-
-
-class EmploeyeSection(models.Model):
-    emp_id = models.ForeignKey(Emploeye, on_delete=models.CASCADE)
-    group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
-    sec_id = models.ForeignKey(Section, on_delete=models.CASCADE)
-    semester = models.CharField(max_length=10, null=True)
-    year = models.CharField(max_length=10, null=True)
-    grade = models.CharField(max_length=6, null=True)
-
-
-class Instructor(models.Model):
-    instrustor_id = models.CharField(primary_key=True, max_length=10)
-    instrustor_name = models.CharField(max_length=50, null=True)
-    dept_name = models.ForeignKey(Department, on_delete=models.CASCADE)
-    salary = models.IntegerField(null=True)
+class Rank(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rank', null=True, blank=True)
+    title = models.CharField(max_length=35)
+    rank_img = models.ImageField(null=True, blank=True, upload_to='images/rank/')
 
     def __str__(self):
-        return self.instrustor_id
+        return self.title
 
 
-class GroupInstructor(models.Model):
-    instructor_id = models.ForeignKey(Instructor, on_delete=models.CASCADE)
-    group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
-    sec_id = models.ForeignKey(Section, on_delete=models.CASCADE)
-    semester = models.CharField(max_length=10, null=True)
-    year = models.CharField(max_length=10, null=True)
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField(null=True, blank=True)
+    profile_pic = models.ImageField(null=True, blank=True, upload_to='images/profile/')
+    score = models.IntegerField(default=0)
+    rank = models.ForeignKey(Rank, on_delete=models.CASCADE, null=True, blank=False)
+
+    def __str__(self):
+        return str(self.user)
